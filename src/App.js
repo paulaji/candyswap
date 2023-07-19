5f1d7be69c901a620e9fc1c57fa365984cb283dc
paulaji <paulajiparayil123@gmail.com>
2023-07-19 16:39:54 +0530

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useEffect, useState } from "react";
+
 const width = 8;
 const candyColors = ["red", "blue", "orange", "green", "purple", "yellow"];
 
 const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
   // function to create the board
   const createBoard = () => {
+    // declaring array to store randomColors
+    const randomColorArrangement = [];
     // we are going to create an 8x8 sized board (widthxwidth) where each square in the board will have a random color picked out of the candyColors array
     // running the loop 64 times so that each time random colors are generated
     for (let i = 0; i < width * width; i++) {
@@ -13,8 +19,17 @@ const App = () => {
       );
       // this will pick a random color based on previous function's number output which will  be passed as index
       const randomColor = candyColors[randomNumberFrom0to5];
+      // now we need to store these random colors as an array
+      randomColorArrangement.push(randomColor);
     }
+    setCurrentColorArrangement(randomColorArrangement);
   };
+  // useEffect called with [] so that createBoard() is called only once
+  // if I were to pass another arguement inside [], say [width], the useEffect will get triggered once width changesS
+  useEffect(() => {
+    createBoard();
+  }, []);
+  console.log(currentColorArrangement);
   return;
 };
 
